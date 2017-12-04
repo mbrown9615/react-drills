d561aedba048d9f5b41c6fd373afc7b45a294559
Mason Brown <masonbrown@Masons-MacBook-Pro.local>
2017-12-04 18:28:12 -0500

    app-6 need more react practice

--- app-6/src/App.js
+++ app-6/src/App.js
@@ -1,18 +1,47 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todo from './Todo.js'
 
 class App extends Component {
+
+constructor(){
+  super();
+
+  this.state = {
+    input: '',
+    list: []
+  }
+  this.handleAddTask = this.handleAddTask.bind( this );
+}
+
+handleInputChange(value){
+  this.setState({
+    input: value
+  })
+}
+
+handleAddTask(value){
+    this.setState({
+      list: [...this.state.list, this.state.input ],
+      input: ''
+    })
+}
+
   render() {
+
+    let list = this.state.list.map( ( element, index ) => {
+      return (
+        <Todo key={ index } task={ element } />
+      )
+    })
+
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header>My To-Do List</header>
+        <input onChange={(e)=> this.handleInputChange(e.target.value)}/>
+        <button onClick={this.handleAddTask}>Add</button>
+
+        {list}
       </div>
     );
   }
