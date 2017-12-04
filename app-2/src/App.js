386e32cea0b8cd06cf7cc2d2d0e222506b15362a
Mason Brown <masonbrown@Masons-MacBook-Pro.local>
2017-12-04 17:12:31 -0500

    app-2 done, messed upapp-1

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+  
+  constructor(){
+    super();
+  
+    this.state = {
+      filterString: "",
+      stuff : ['food','games','work','sports']
+    }
   }
-}
+  
+  handleChange(filter){
+    this.setState({filterString: filter})
+  }
+  
+    render() {
+      let stuffToDisplay = this.state.stuff.filter( (element, index) => {
+        return element.includes( this.state.filterString );
+      }).map( (element, index) => {
+        return <h2 key={ index }>{ element }</h2>
+      })
+  
+      return (
+          <div>
+          <input onChange={(e)=>this.handleChange(e.target.value)}/>
+        {stuffToDisplay}
+        </div>
+      );
+    }
+  }
 
 export default App;
